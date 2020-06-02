8d6fc45c905fc569686fffc91fa67adf25f786ab
unknown <yonish3@gmail.com>
2020-06-02 16:38:50 +0300

    bundung

--- public/customActivity.js
+++ public/customActivity.js
@@ -9,5 +9,6 @@ connection.on('initActivity', function(payload) {
 connection.on('clickedNext', function() { 
     console.log('the next button has been clicked?')
     var payload = 'test payload'
-    connection.trigger('updateActivity', payload);
+    // connection.trigger('updateActivity', payload);
+
  });
