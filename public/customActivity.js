a00c409ebc61c93f54cea62e11108f2d47727433
unknown <yonish3@gmail.com>
2020-06-02 17:26:30 +0300

    bundung

--- public/customActivity.js
+++ public/customActivity.js
@@ -3,33 +3,40 @@ var connection = new Postmonger.Session()
 connection.trigger('ready');
 var payload = `
 {
-	"workflowApiVersion": "1.1",
-	"metaData": {
-		"icon": "ServiceWise-Logo-512.png",
-		"category": "message"
-	},
-	"type": "REST",
-	"userInterfaces": {
-		"configModal": {
-			"height": 200,
-			"width": 300,
-			"fullscreen": true
-		}
-	},
+    "name": "",
+    "id": null,
+    "key": "REST-1",
     "arguments": {
-        "execute": {
-            "inArguments": [
-                {
-                    "contactIdentifier": "{{Contact.Key}}"
-                },
-                {
-                    "emailAddress": "{{InteractionDefaults.Email}}"
-				}
-			]
-		}
-	},
-	"url": "https://518f99cb594e9d5daf1228f9f1d35a7a.m.pipedream.net"
-}`
+      "execute": {
+        "inArguments": [
+          { "contactKey": "{{Contact.Key}}" },
+          { "FirstName": "{{Contact.Attribute.JourneyEntrySource.FirstName}}"},
+          { "LastName": "{{Contact.Attribute.JourneyEntrySource.LastName}}"}
+        ],
+        "url": "https://518f99cb594e9d5daf1228f9f1d35a7a.m.pipedream.net"
+      }
+    },
+    "configurationArguments": {
+      "publish": {
+        "url": "https://518f99cb594e9d5daf1228f9f1d35a7a.m.pipedream.net"
+      }
+    },
+    "metaData": {
+      "icon": "https://sample-activity.herokuapp.com/icon.png",
+      "category": "message",
+      "iconSmall": null,
+      "original_icon": "icon.png",
+      "isConfigured": true
+    },
+    "editable": true,
+    "outcomes": [
+      {
+        "next": "WAITBYDURATION-1"
+      }
+    ],
+    "errors": []
+  }
+`
 
 var config = JSON.parse(payload)
 connection.on('initActivity', function(payload) {
@@ -39,5 +46,4 @@ connection.on('initActivity', function(payload) {
 connection.on('clickedNext', function() { 
     console.log('the next button has been clicked?')
     connection.trigger('updateActivity', config);
-
  });
