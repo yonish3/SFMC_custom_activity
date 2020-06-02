c4b7eee78fd5af66aca83edab50758a75d4f7330
unknown <yonish3@gmail.com>
2020-06-02 13:06:29 +0300

    logo

--- public/customActivity.js
+++ public/customActivity.js
@@ -3,7 +3,7 @@ var connection = new Postmonger.Session()
 connection.trigger('ready');
 
 connection.on('initActivity', function(payload) {
-    document.querySelector("#MC_data").innerText = 'rrrrrrr'
+    document.querySelector("#MC_data").innerText = 'hello hello'
 });
 
 connection.on('clickedNext', function() { 
