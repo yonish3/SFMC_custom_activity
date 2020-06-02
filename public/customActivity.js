2c63b03265126bba385008b37e820d662379de9b
unknown <yonish3@gmail.com>
2020-06-02 17:03:33 +0300

    bundung

--- public/customActivity.js
+++ public/customActivity.js
@@ -1,6 +1,35 @@
 var connection = new Postmonger.Session()
 
 connection.trigger('ready');
+var payload = `
+{
+	"workflowApiVersion": "1.1",
+	"metaData": {
+		"icon": "ServiceWise-Logo-512.png",
+		"category": "message"
+	},
+	"type": "REST",
+	"userInterfaces": {
+		"configModal": {
+			"height": 200,
+			"width": 300,
+			"fullscreen": true
+		}
+	},
+    "arguments": {
+        "execute": {
+            "inArguments": [
+                {
+                    "contactIdentifier": "{{Contact.Key}}"
+                },
+                {
+                    "emailAddress": "{{InteractionDefaults.Email}}"
+				}
+			]
+		}
+	},
+	"url": "https://518f99cb594e9d5daf1228f9f1d35a7a.m.pipedream.net"
+}`
 
 connection.on('initActivity', function(payload) {
     document.querySelector("#MC_data").innerText = 'hello hello'
@@ -8,7 +37,6 @@ connection.on('initActivity', function(payload) {
 
 connection.on('clickedNext', function() { 
     console.log('the next button has been clicked?')
-    var payload = 'test payload'
-    // connection.trigger('updateActivity', payload);
+    connection.trigger('updateActivity', payload);
 
  });
