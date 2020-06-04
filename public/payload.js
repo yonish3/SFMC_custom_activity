6c4ef1e8cb3169c8ad9fb575ad1b7657b0499704
unknown <yonish3@gmail.com>
2020-06-04 14:22:02 +0300

    clean config to send

--- public/payload.js
+++ public/payload.js
@@ -12,5 +12,19 @@ var payload = {
     },
     "metaData": {
       "isConfigured": true
+    },
+    "configurationArguments": {
+      "save": {
+        "url": "https://518f99cb594e9d5daf1228f9f1d35a7a.m.pipedream.net"
+      },
+      "publish": {
+        "url": "https://518f99cb594e9d5daf1228f9f1d35a7a.m.pipedream.net"
+      },
+      "validate": {
+        "url": "https://518f99cb594e9d5daf1228f9f1d35a7a.m.pipedream.net"
+      },
+      "stop": {
+        "url": "https://518f99cb594e9d5daf1228f9f1d35a7a.m.pipedream.net"
+      }
     }
   }
