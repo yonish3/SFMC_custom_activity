00364d138d702bf9fe36556e04df877405e7939f
unknown <yonish3@gmail.com>
2020-06-04 14:48:21 +0300

    tokens test 3

--- public/customActivity.js
+++ public/customActivity.js
@@ -10,12 +10,21 @@ connection.on('initActivity', function() {
 });
 
 connection.trigger('requestTokens');
+connection.trigger('requestEndpoints');
+
+connection.on('requestedEndpoints', onGetEndpoints);
 connection.on('requestedTokens', onGetTokens);
 
 function onGetTokens (tokens) {
   // Response: tokens = { token: <legacy token>, fuel2token: <fuel api token> }
   console.log('tokens: ',tokens);
 }
+
+function onGetEndpoints (endpoints) {
+  // Response: endpoints = { restHost: <url> } i.e. "rest.s1.qa1.exacttarget.com"
+  console.log(endpoints);
+}
+
 
 connection.on('clickedNext', function() { 
     console.log('the next button has been clicked?')
