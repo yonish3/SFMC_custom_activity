1e96a25504bd15196854a80e252899829b881811
unknown <yonish3@gmail.com>
2020-06-04 11:04:44 +0300

    garbeg

--- public/customActivity.js
+++ public/customActivity.js
@@ -5,6 +5,42 @@ connection.trigger('ready');
 
 // var config = JSON.parse(payload)
 var configToShow = JSON.stringify(configToSend)
+{
+    "name": "",
+    "id": null,
+    "key": "REST-1",
+    "arguments": {
+      "execute": {
+        "inArguments": [
+                { "contactKey": "{{Contact.Key}}" },
+                { "FirstName": "{{Contact.Attribute.JourneyEntrySource.FirstName}}"},
+                { "FirstName": "{{Contact.Attribute.JourneyEntrySource.FirstName}}"},
+                { "LastName": "{{Contact.Attribute.JourneyEntrySource.LastName}}"}
+        ],
+        "url": "https://518f99cb594e9d5daf1228f9f1d35a7a.m.pipedream.net"
+      }
+    },
+    "configurationArguments": {
+      "publish": {
+        "url": "https://518f99cb594e9d5daf1228f9f1d35a7a.m.pipedream.net"
+      }
+    },
+    "metaData": {
+      "icon": "https://sample-activity.herokuapp.com/icon.png",
+      "category": "message",
+      "iconSmall": null,
+      "original_icon": "icon.png",
+      "isConfigured": true
+    },
+    "editable": true,
+    "outcomes": [
+      {
+        "next": "WAITBYDURATION-1"
+      }
+    ],
+    "errors": []
+  }
+`
 
 connection.on('initActivity', function(configToShow) {
     document.querySelector("#MC_data").innerText = configToShow
