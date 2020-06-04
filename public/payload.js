4dd717b224af151ebca43c9c3821f0e97315c318
unknown <yonish3@gmail.com>
2020-06-04 13:04:07 +0300

    clean config to send

--- public/payload.js
+++ public/payload.js
@@ -9,15 +9,5 @@ var payload = {
         ],
         "url": "https://518f99cb594e9d5daf1228f9f1d35a7a.m.pipedream.net"
       }
-    },
-    "metaData": {
-      "isConfigured": true
-    },
-    "editable": true,
-    "outcomes": [
-      {
-        "next": "WAITBYDURATION-1"
-      }
-    ],
-    "errors": []
+    }
   }
