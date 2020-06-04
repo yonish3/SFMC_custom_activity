99a6f65b25066e7b41f59f07b19aaf8670e964b6
unknown <yonish3@gmail.com>
2020-06-04 14:34:58 +0300

    config file is working

--- public/customActivity.js
+++ public/customActivity.js
@@ -5,9 +5,10 @@ console.log(payload)
 var config = JSON.stringify(payload)
 
 
-connection.on('initActivity', function(payload) {
+connection.on('initActivity', function() {
     document.querySelector("#MC_data").innerText = config
 });
+
 
 connection.on('clickedNext', function() { 
     console.log('the next button has been clicked?')
