b5e3d307da58594535231a37e11a250a13f97a4f
unknown <yonish3@gmail.com>
2020-06-04 11:35:36 +0300

    working

--- public/customActivity.js
+++ public/customActivity.js
@@ -1,52 +1,14 @@
 var connection = new Postmonger.Session()
 
 connection.trigger('ready');
-var payload = `
-
-{
-    "name": "",
-    "id": null,
-    "key": "REST-1",
-    "arguments": {
-      "execute": {
-        "inArguments": [
-                { "contactKey": "{{Contact.Key}}" },
-                { "FirstName": "{{Contact.Attribute.JourneyEntrySource.FirstName}}"},
-                { "LastName": "{{Contact.Attribute.JourneyEntrySource.LastName}}"},
-                { "emailAddress": "{{InteractionDefaults.Email}}" }
-        ],
-        "url": "https://518f99cb594e9d5daf1228f9f1d35a7a.m.pipedream.net"
-      }
-    },
-    "configurationArguments": {
-      "publish": {
-        "url": "https://518f99cb594e9d5daf1228f9f1d35a7a.m.pipedream.net"
-      }
-    },
-    "metaData": {
-      "icon": "https://sample-activity.herokuapp.com/icon.png",
-      "category": "message",
-      "iconSmall": null,
-      "original_icon": "icon.png",
-      "isConfigured": true
-    },
-    "editable": true,
-    "outcomes": [
-      {
-        "next": "WAITBYDURATION-1"
-      }
-    ],
-    "errors": []
-  }
-`
-
-var config = JSON.parse(payload)
+console.log(configToSend)
+var config = JSON.stringify(configToSend)
 
 connection.on('initActivity', function(payload) {
-    document.querySelector("#MC_data").innerText = payload
+    document.querySelector("#MC_data").innerText = config
 });
 
 connection.on('clickedNext', function() { 
     console.log('the next button has been clicked?')
-    connection.trigger('updateActivity', config);
+    connection.trigger('updateActivity', configToSend);
  });
