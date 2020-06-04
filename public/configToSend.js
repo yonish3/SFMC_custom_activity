15877349f1141b9a2e33d399ed5030947ee8ac55
unknown <yonish3@gmail.com>
2020-06-04 12:20:43 +0300

    clean config to send

--- public/configToSend.js
+++ public/configToSend.js
@@ -12,24 +12,5 @@ var configToSend = {
         ],
         "url": "https://518f99cb594e9d5daf1228f9f1d35a7a.m.pipedream.net"
       }
-    },
-    "configurationArguments": {
-      "publish": {
-        "url": "https://518f99cb594e9d5daf1228f9f1d35a7a.m.pipedream.net"
-      }
-    },
-    "metaData": {
-      "icon": "https://sample-activity.herokuapp.com/icon.png",
-      "category": "message",
-      "iconSmall": null,
-      "original_icon": "icon.png",
-      "isConfigured": true
-    },
-    "editable": true,
-    "outcomes": [
-      {
-        "next": "WAITBYDURATION-1"
-      }
-    ],
-    "errors": []
+    }
   }
