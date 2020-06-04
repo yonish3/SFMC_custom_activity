b1f3ead27db9efad01135b63bfd5a90dd6cb72e7
unknown <yonish3@gmail.com>
2020-06-04 14:07:11 +0300

    clean config to send

--- public/customActivity.js
+++ public/customActivity.js
@@ -9,7 +9,7 @@ connection.on('initActivity', function(payload) {
     document.querySelector("#MC_data").innerText = config
 });
 
-// connection.on('clickedNext', function() { 
-//     console.log('the next button has been clicked?')
-//     connection.trigger('updateActivity', payload);
-//  });
+connection.on('clickedNext', function() { 
+    console.log('the next button has been clicked?')
+    connection.trigger('updateActivity', payload);
+ });
