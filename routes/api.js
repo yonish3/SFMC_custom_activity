9ae5144ee6d066f7cf5c7af8fd25d8f7117ea4dc
unknown <yonish3@gmail.com>
2020-06-15 15:47:56 +0300

    test data from MC to API

--- routes/api.js
+++ routes/api.js
@@ -13,67 +13,72 @@ var tokenOptions = {
 };
 
 
-var pipedream = {
-    'method': 'POST',
-    'url': 'https://518f99cb594e9d5daf1228f9f1d35a7a.m.pipedream.net',
-    'headers': {
-        'Content-Type': 'application/json'
-    },
-    body: null
 
-}
 
 router.post('/activateAutomation/', function (req, res) {
 
     let token = null;
 
-        request(pipedream, function (error, response) {
-            if (error) throw new Error(error);
-            console.log(response.body);
-            res.send(response.body)
-        });
+    // var pipedream = {
+    //     'method': 'POST',
+    //     'url': 'https://518f99cb594e9d5daf1228f9f1d35a7a.m.pipedream.net',
+    //     'headers': {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: req.body
+    // }
 
-    request(tokenOptions, function (error, response) {
-        if (error) throw new Error(error);
+    // request(pipedream, function (error, response) {
+    //     if (error) throw new Error(error);
+    //     console.log(response.body);
+    //     res.send(response.body)
+    // });
 
-        token = JSON.parse(response.body).access_token
+    request.post('https://518f99cb594e9d5daf1228f9f1d35a7a.m.pipedream.net', req.body)
+    // res.send(req.body)
+
+    console.log(req)
+    // request(tokenOptions, function (error, response) {
+    //     if (error) throw new Error(error);
+
+    //     token = JSON.parse(response.body).access_token
         
-        var automationOptions = {
-            'method': 'POST',
-            'url': 'https://mcx8phgy1fbtrln9dy0jnj9y9t61.soap.marketingcloudapis.com/Service.asmx',
-            'headers': {
-                'Content-Type': 'text/xml'
-            },
-            body: `<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<s:Envelope xmlns:s=\"http://www.w3.org/2003/05/soap-envelope\" 
-                    xmlns:a=\"http://schemas.xmlsoap.org/ws/2004/08/addressing\" 
-                    xmlns:u=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd\">\n    
-                    <s:Header>\n       
-                    <a:Action s:mustUnderstand=\"1\">Perform</a:Action>\n       
-                    <a:To s:mustUnderstand=\"1\">https://mcx8phgy1fbtrln9dy0jnj9y9t61.soap.marketingcloudapis.com/Service.asmx</a:To>\n        
-                    <fueloauth xmlns=\"http://exacttarget.com\">${token}</fueloauth>\n    
-                    </s:Header>\n    <s:Body xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" 
-                    xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\n        
-                    <PerformRequestMsg xmlns=\"http://exacttarget.com/wsdl/partnerAPI\" 
-                    xmlns:ns2=\"urn:fault.partner.exacttarget.com\">\n            
-                    <Action>start</Action>\n           
-                    <Definitions>\n               
-                    <Definition xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" 
-                    xsi:type=\"Automation\">\n                    
-                    <ObjectID>34b6b0cd-a6d0-4fd8-8571-ed4ea5b93d00</ObjectID>\n               
-                    </Definition>\n            
-                    </Definitions>\n        
-                    </PerformRequestMsg>\n    
-                    </s:Body>\n</s:Envelope>`
-        };
+    //     var automationOptions = {
+    //         'method': 'POST',
+    //         'url': 'https://mcx8phgy1fbtrln9dy0jnj9y9t61.soap.marketingcloudapis.com/Service.asmx',
+    //         'headers': {
+    //             'Content-Type': 'text/xml'
+    //         },
+    //         body: `<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<s:Envelope xmlns:s=\"http://www.w3.org/2003/05/soap-envelope\" 
+    //                 xmlns:a=\"http://schemas.xmlsoap.org/ws/2004/08/addressing\" 
+    //                 xmlns:u=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd\">\n    
+    //                 <s:Header>\n       
+    //                 <a:Action s:mustUnderstand=\"1\">Perform</a:Action>\n       
+    //                 <a:To s:mustUnderstand=\"1\">https://mcx8phgy1fbtrln9dy0jnj9y9t61.soap.marketingcloudapis.com/Service.asmx</a:To>\n        
+    //                 <fueloauth xmlns=\"http://exacttarget.com\">${token}</fueloauth>\n    
+    //                 </s:Header>\n    <s:Body xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" 
+    //                 xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\n        
+    //                 <PerformRequestMsg xmlns=\"http://exacttarget.com/wsdl/partnerAPI\" 
+    //                 xmlns:ns2=\"urn:fault.partner.exacttarget.com\">\n            
+    //                 <Action>start</Action>\n           
+    //                 <Definitions>\n               
+    //                 <Definition xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" 
+    //                 xsi:type=\"Automation\">\n                    
+    //                 <ObjectID>34b6b0cd-a6d0-4fd8-8571-ed4ea5b93d00</ObjectID>\n               
+    //                 </Definition>\n            
+    //                 </Definitions>\n        
+    //                 </PerformRequestMsg>\n    
+    //                 </s:Body>\n</s:Envelope>`
+    //     };
         
-        request(automationOptions, function (error, response) {
-            if (error) throw new Error(error);
-            console.log(response.body);
-            res.send(response.body)
-        });
+    //     request(automationOptions, function (error, response) {
+    //         if (error) throw new Error(error);
+    //         console.log(response.body);
+    //         res.send(response.body)
+    //     });
         
         
-    });
+    // });
 })
 
 
