a971cd774b9b776d21a498221094d8e4c711c8c3
unknown <yonish3@gmail.com>
2020-07-05 15:00:15 +0300

    v

--- public/payload.js
+++ public/payload.js
@@ -2,10 +2,12 @@ var payload = {
   "arguments": {
       "execute": {
         "inArguments": [
-                { "contactKey": "{{Contact.Key}}" },
-                { "FirstName": "{{Contact.Attribute.JourneyEntrySource.FirstName}}"},
-                { "LastName": "{{Contact.Attribute.JourneyEntrySource.LastName}}"},
-                { "emailAddress": "{{InteractionDefaults.Email}}" }
+          { "contactKey": "{{Contact.Key}}" },
+          { "FirstName": "{{Contact.Attribute.JourneyEntrySource.FirstName}}"},
+          { "LastName": "{{Contact.Attribute.JourneyEntrySource.LastName}}"},
+          { "emailAddress": "{{InteractionDefaults.Email}}" },
+          { "test_f": "{{test_f}}"},
+          { "First Name": "{{Contact.Attribute.[Email Demographics].[First Name]}}"}
         ],
         "url": "https://sfmc-custom-activity-test.herokuapp.com/activateAutomation"
       }
