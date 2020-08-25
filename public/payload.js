96c9cf4fbecb51ccd84c035b87f50de18c9adef2
unknown <yonish3@gmail.com>
2020-08-25 15:51:16 +0300

    fix logo

--- public/payload.js
+++ public/payload.js
@@ -6,7 +6,6 @@ var payload = {
           { "FirstName": "{{Contact.Attribute.JourneyEntrySource.FirstName}}"},
           { "LastName": "{{Contact.Attribute.JourneyEntrySource.LastName}}"},
           { "emailAddress": "{{InteractionDefaults.Email}}"},
-          { "First Name": "{{Contact.Attribute.93EA27F6-1953-4678-958F-F8FD35CCA48B.test_f}}"},
           { "First Name": "{{Contact.Attribute.custom_activity_DE.test_f}}"}
         ],
         "url": "https://518f99cb594e9d5daf1228f9f1d35a7a.m.pipedream.net"
